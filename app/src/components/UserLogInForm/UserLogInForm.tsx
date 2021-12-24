27cab40299c9c5d235d8deec3a7254a4f106ed8e
Ulad Birukou <allorine99@gmail.com>
2021-12-24 17:55:45 +0300

    develop-add-validation&add-errors-style

--- app/src/components/UserLogInForm/UserLogInForm.tsx
+++ app/src/components/UserLogInForm/UserLogInForm.tsx
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef, forwardRef } from "react";
 import { Formik, FormikHelpers, Form, Field } from "formik";
 import { UserLogin } from "../UserLogInForm/utils/interfaces/interfaces";
 import * as yup from "yup";
 import { Paths } from "../../paths/path";
 import { useNavigate } from "react-router-dom";
-import { XIcon } from "@heroicons/react/solid";
+import {
+  XIcon,
+  MailIcon,
+  EyeIcon,
+  EyeOffIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/solid";
 import axios from "axios";
 
 const UserLogInForm = () => {
-  const [popUpStatus, setPopUpStatus] = useState<boolean>(true);
+  const [popUpStatus, setPopUpStatus] = useState<boolean>(false);
   const [checked, setChecked] = useState<boolean>(false);
+  const [visiblePassword, setVisiblePassword] = useState<boolean>(false);
+  const [userInformation, setUserInformation] = useState<object>({
+    email: "",
+    password: "",
+  });
   const [token, setToken] = useState(null);
+
+  const refContain = useRef(null);
 
   const submitUserInformation = async (values: UserLogin) => {
     try {
       const data = await axios.post(
-        `http://localhost:5000${Paths.LOG_IN}`,
+        `http://localhost:8000${Paths.LOG_IN}`,
         values
       );
 
@@ -35,7 +48,13 @@ const UserLogInForm = () => {
 
   let history = useNavigate();
 
-  const validationSchema = yup.object().shape({});
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Please add correct email")
+      .required("Please add email"),
+    password: yup.string().required("Please add correct password"),
+  });
 
   const handlePopUp = () => {
     setPopUpStatus((prev) => !prev);
@@ -48,6 +67,16 @@ const UserLogInForm = () => {
   const handleRoutePage = () => {
     history(Paths.SIGN_UP);
   };
+
+  const handleVisiblePasswordStatus = () => {
+    setVisiblePassword((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (visiblePassword) {
+      console.log(refContain.current);
+    }
+  }, [visiblePassword]);
 
   return (
     <div className="w-screen flex justify-center">
@@ -64,11 +93,15 @@ const UserLogInForm = () => {
         ) => {
           setTimeout(() => {
             submitUserInformation(values);
+            setUserInformation({
+              email: values.email,
+              password: values.password,
+            });
             setSubmitting(false);
           }, 500);
         }}
       >
-        {({ errors, touched, handleChange, handleBlur }) => (
+        {({ errors, touched, isValid, dirty, handleChange, handleBlur }) => (
           <div
             className={
               popUpStatus
@@ -104,13 +137,31 @@ const UserLogInForm = () => {
                   <Form className="mt-8 space-y-6" action="#">
                     <input type="hidden" name="remember" defaultValue="true" />
                     <div className="rounded-md">
-                      <div className="mb-5">
+                      <div className="mb-5 relative">
                         <label
                           htmlFor="email-address"
                           className="text-xs text-gray-700"
                         >
                           Email
                         </label>
+                        <div className="absolute left-0 pl-3 z-10 top-8 flex items-center pointer-event-none">
+                          <MailIcon className="h-6 w-5" fill="#9CA3AF" />
+                        </div>
+                        {!isValid && !dirty ? (
+                          <>
+                            <div className="absolute left-0 pl-3 z-10 top-8 flex items-center pointer-event-none">
+                              <MailIcon className="h-6 w-5" fill="#EF4444" />
+                            </div>
+                            <div className="absolute left-96 pl-3 z-10 top-8 flex items-center pointer-event-none">
+                              <ExclamationCircleIcon
+                                className="h-6 w-5"
+                                fill="#EF4444"
+                              />
+                            </div>
+                          </>
+                        ) : (
+                          <></>
+                        )}
                         <Field
                           id="email-address"
                           name="email"
@@ -119,31 +170,64 @@ const UserLogInForm = () => {
                           onBlur={handleBlur}
                           autoComplete="email"
                           required
-                          className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          className={
+                            !isValid && !dirty
+                              ? "appearance-none rounded-md relative block w-full pl-10 pr-14 px-3 py-2 border border-red-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500  sm:text-sm"
+                              : "appearance-none rounded-md relative block w-full pl-10 pr-14 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          }
                           placeholder="you@example.com"
                         />
-                        {touched.email && errors.email && <p>{errors.email}</p>}
+                        {touched.email && errors.email && (
+                          <p className="text-red-500 text-xs">{errors.email}</p>
+                        )}
                       </div>
-                      <div>
+                      <div className="relative">
                         <label
                           htmlFor="password"
                           className="text-xs text-gray-700"
                         >
                           Password
                         </label>
+                        <button
+                          onClick={handleVisiblePasswordStatus}
+                          className="absolute left-96 pl-3 z-10 top-8 flex items-center pointer-event-none"
+                        >
+                          {visiblePassword ? (
+                            <EyeOffIcon className="h-6 w-5" fill="#9CA3AF" />
+                          ) : (
+                            <EyeIcon className="h-6 w-5" fill="#9CA3AF" />
+                          )}
+                        </button>
+                        {!isValid && !dirty ? (
+                          <>
+                            <div className="absolute left-3/4 pl-3 z-10 top-8 flex items-center pointer-event-none">
+                              <ExclamationCircleIcon
+                                className="h-6 w-5"
+                                fill="#EF4444"
+                              />
+                            </div>
+                          </>
+                        ) : (
+                          <></>
+                        )}
                         <Field
                           id="password"
                           name="password"
-                          type="password"
                           onChange={handleChange}
                           onBlur={handleBlur}
                           autoComplete="current-password"
                           required
-                          className="appearance-none rounded-md relative block w-full px-3 py-2 border   border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                          className={
+                            !isValid && !dirty
+                              ? "appearance-none rounded-md relative block w-full px-3 py-2 border border-red-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500  sm:text-sm"
+                              : "appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                          }
                           placeholder="Enter Password"
                         />
                         {touched.password && errors.password && (
-                          <p>{errors.password}</p>
+                          <p className="text-red-500 text-xs">
+                            {errors.password}
+                          </p>
                         )}
                       </div>
                     </div>
@@ -176,7 +260,12 @@ const UserLogInForm = () => {
                     <div>
                       <button
                         type="submit"
-                        className="group relative w-full flex justify-center py-2.5 px-4 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-gray-200"
+                        disabled={!isValid && !dirty}
+                        className={
+                          isValid && dirty
+                            ? "group relative w-full flex justify-center py-2.5 px-4 border text-white border-transparent text-sm font-medium rounded-md text-gray-500 bg-blue-600 leading-5"
+                            : "group relative w-full flex justify-center py-2.5 px-4 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-gray-200 leading-5"
+                        }
                       >
                         Log in
                       </button>
