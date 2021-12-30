ddb00a288cdf3785e721ee7210b1079d1b8b3408
Ivan Uglovets <uglovec@gmail.com>
2021-12-30 16:12:50 +0300

    start of development objectCard

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ObjectCard from "./components/ObjectCard/ObjectCard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Start your application here!
-        </a>
-      </header>
+      <ObjectCard />
     </div>
   );
 }
