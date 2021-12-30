8b4848b27ab792f1166bff9d0c961b07ba7f98df
Bondarenko Jenya <bond.jenek@gmail.com>
2021-12-30 08:01:29 +0000

    Apartment model update and definition of the download function
    Adding new fields to the apartment model. Uploading pictures to the file storage of the project. Documenting created classes and methods. Adding a function to load data into the database from a csv file.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +30,6 @@ urlpatterns = [
                                         url_name='openapi-schema'),
          name='swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
