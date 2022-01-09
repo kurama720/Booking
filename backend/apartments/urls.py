1853a3e379c9324aadac47c0d89c3ef2a79df2af
slava <kindeev101048@gmail.com>
2022-01-09 23:47:18 +0300

    Fix code after megre

--- backend/apartments/urls.py
+++ backend/apartments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from apartments.api.views import ApartmentListView
+from apartments.api.views import ApartmentViewSet
 from apartments.views import BookingView
 
 urlpatterns = [
     path('<int:pk>/book', BookingView.as_view()),
-    path('', ApartmentListView.as_view({'get': 'list'}), name='apartment_filter'),
+    path('', ApartmentViewSet.as_view({'get': 'list'}), name='apartment_filter'),
 ]
