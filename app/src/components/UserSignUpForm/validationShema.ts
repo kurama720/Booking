bcc06566e1ebdc5f53879ace3e8d74b62303647a
Ivan Uglovets <uglovec@gmail.com>
2022-01-13 15:40:40 +0300

    fix/bugs-validation-password

--- app/src/components/UserSignUpForm/validationShema.ts
+++ app/src/components/UserSignUpForm/validationShema.ts
@@ -8,8 +8,8 @@ export const validationSchema = yup.object().shape({
         .min(2, 'The minimum letters be 2!')
         .max(32, 'The maximum letters be 32!').required('Last Name is required!'),
     email: yup.string().email('You’ve entered invalid e-mail!').required('E-mail is required!'),
-    password: yup.string().matches(/^((?=.*\d)(?=.*[a-zA-Z]).{8,})$/,
-        "Password must be at least 8 characters, letters and numbers!"
+    password: yup.string().matches(/^((?=^\S+$)(?=.*\d)(?=.*[a-zA-Z]).{8,})$/,
+        "Password must be at least 8 characters, letters, numbers and no spaces!"
     ).required('The Password is required'),
     confirm_password: yup.string().oneOf([yup.ref('password')], "The passwords don't match!").required('Confirm Password is required!')
 })
