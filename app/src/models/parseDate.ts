ed4ede75c5b362b541855a18da4f40548f2b0ea2
Ivan Uglovets <uglovec@gmail.com>
2022-01-19 16:15:00 +0300

    fix/bugs-in-date

--- app/src/models/parseDate.ts
+++ app/src/models/parseDate.ts
@@ -1,6 +1,6 @@
 export const parseDate = (date: Date | null) => {
     if (date) {
-        date.setDate(date.getDate());
+        date.setDate(date.getDate() + 1);
         return date.toISOString().substring(0, 10);
     }
 }
