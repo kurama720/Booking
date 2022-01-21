c0374eb4512ca250ec3248246545b0f16c83d176
Ivan Uglovets <uglovec@gmail.com>
2022-01-21 15:51:27 +0300

    fix/style-code

--- app/src/components/ApartmentForm/ApartmentForm.tsx
+++ app/src/components/ApartmentForm/ApartmentForm.tsx
@@ -6,17 +6,17 @@ import {DateRange} from "@mui/lab/DateRangePicker";
 import {parseDate} from "../../models/parseDate";
 
 const ApartmentForm: FC = () => {
-    const [value, setValue] = React.useState<DateRange<Date>>([null, null]);
-    const [quantityGuests, setQuantityGuests] = useState<number>(1)
+    const [valueDate, setValueDate] = React.useState<DateRange<Date>>([null, null]);
+    const [numberGuests, setNumberGuests] = useState<number>(1)
     const [isShowGuestsWindow, setIsShowGuestsWindow] = useState<boolean>(false)
     const [serverErrors, setServerErrors] = useState(null)
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         const dataForReserve: dataForApartmentForm = {
-            check_in: parseDate(value[0]),
-            check_out: parseDate(value[1]),
-            guests: quantityGuests
+            check_in: parseDate(valueDate[0]),
+            check_out: parseDate(valueDate[1]),
+            guests: numberGuests
         }
     }
 
@@ -25,11 +25,11 @@ const ApartmentForm: FC = () => {
     }
 
     const incrementGuests = (): void => {
-        setQuantityGuests(prev => prev + 1)
+        setNumberGuests(prev => prev + 1)
     }
 
     const decrementGuests = (): void => {
-        setQuantityGuests(prev => prev - 1)
+        setNumberGuests(prev => prev - 1)
     }
 
     return (
@@ -43,16 +43,16 @@ const ApartmentForm: FC = () => {
                     <div className='flex items-center'>
                         <span><StarIcon className='text-blue-500 w-4 h-4'/></span>
                         <span className='text-sm font-body'>4,3</span>
-                        <span className='inline-block  mx-2 w-0.5 h-0.5 bg-gray-700 rounded-full'/>
+                        <span className='inline-block mx-2 w-0.5 h-0.5 bg-gray-700 rounded-full'/>
                         <span className='text-blue-600 text-sm font-body font-medium'>15 reviews</span>
                     </div>
                 </div>
                 <PartFormApartment
-                    valueDate={value}
-                    setValueDate={setValue}
-                    setQuantityGuests={setQuantityGuests}
+                    valueDate={valueDate}
+                    setValueDate={setValueDate}
+                    setNumberGuests={setNumberGuests}
                     handleSubmit={handleSubmit}
-                    quantityGuests={quantityGuests}
+                    numberGuests={numberGuests}
                     isShowGuestsWindow={isShowGuestsWindow}
                     decrementGuests={decrementGuests}
                     incrementGuests={incrementGuests}
--- app/src/components/ApartmentForm/PartFormApartment/IPropsPartFormApartment.ts
+++ app/src/components/ApartmentForm/PartFormApartment/IPropsPartFormApartment.ts
@@ -5,8 +5,8 @@ export interface IPropsFormApartment {
     valueDate: DateRange<Date>
     setValueDate: React.Dispatch<React.SetStateAction<DateRange<Date>>>
     handleSubmit:  (e: React.FormEvent<HTMLFormElement>) => void
-    quantityGuests: number
-    setQuantityGuests: React.Dispatch<React.SetStateAction<number>>
+    numberGuests: number
+    setNumberGuests: React.Dispatch<React.SetStateAction<number>>
     isShowGuestsWindow: boolean
     decrementGuests: () => void
     incrementGuests: () => void
