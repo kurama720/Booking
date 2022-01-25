e8c07355b03fe3ea9e97325102a3b3030e875a07
Ivan Plakhin <berlininin7@gmail.com>
2022-01-25 13:27:03 +0000

    Add serializers and view

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from accounts.api.views import RegisterView, CustomTokenObtainView
-from accounts.api.views import LogoutView
-from accounts.api.views import BusinessClientRegisterView
-from accounts.api.views import BusinessClientSignInView
+from accounts.api.views import (RegisterView,
+                                CustomTokenObtainView,
+                                LogoutView,
+                                BusinessClientRegisterView,
+                                BusinessClientSignInView,
+                                UserInfoView)
 
 urlpatterns = [
     path('signup/', RegisterView.as_view(), name='sign_up'),
@@ -12,4 +14,5 @@ urlpatterns = [
     path('business/signup/', BusinessClientRegisterView.as_view(), name='business_sign_up'),
     path('business/signin/', BusinessClientSignInView.as_view(), name='business_sign_in'),
     path('business/logout/', LogoutView.as_view(), name='business_log_out'),
+    path('info/', UserInfoView.as_view(), name='info')
 ]
