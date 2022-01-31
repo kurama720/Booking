606974c99cd3ed767ee3545237db7c5c5cd7f254
Viachaslau Kindzeyeu <kindeev101048@gmail.com>
2022-01-31 13:42:20 +0000

    YT-127. Implement favorite apartments.

--- backend/apartments/urls.py
+++ backend/apartments/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from apartments.api.views import BookingView, ReviewsView, BookingHistoryView, PriceAnalyticView
+from apartments.api.views import (BookingView, ReviewsView, BookingHistoryView, PriceAnalyticView, FavoriteApartmentView)
 
 urlpatterns = [
     path('<int:pk>/book', BookingView.as_view(), name='booking'),
     path('<int:pk>/reviews', ReviewsView.as_view(), name='reviews'),
     path('special/book-history/', BookingHistoryView.as_view(), name="book_history"),
-    path('special/price-analytic/', PriceAnalyticView.as_view({"post": "create"}), name="price_analytic")
+    path('special/price-analytic/', PriceAnalyticView.as_view({"post": "create"}), name="price_analytic"),
+    path('favorite/list', FavoriteApartmentView.as_view({'get': 'list'}), name='favorite_apartment'),
+    path('favorite/<int:pk>/save', FavoriteApartmentView.as_view({'post': 'create'}), name='favorite_apartment_add'),
 ]
