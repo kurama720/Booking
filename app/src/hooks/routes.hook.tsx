a2db4c00f350df820ec3f2454a828c8eaa8a2ffb
Ivan Uglovets <uglovec@gmail.com>
2022-02-08 17:01:30 +0300

    fix/-YT-188-start develop confirm page

--- app/src/hooks/routes.hook.tsx
+++ app/src/hooks/routes.hook.tsx
@@ -4,6 +4,7 @@ import { Paths } from "../paths/path";
 import HomePage from "../pages/HomePage/HomePage";
 import MapSearchPage from "../pages/MapSearchPage";
 import ObjectPage from "../pages/ObjectPage";
+import ConfirmPage from "../pages/ConfirmPage";
 
 export const useRoutes = (isAuth: boolean) => {
   if (isAuth) {
@@ -12,6 +13,7 @@ export const useRoutes = (isAuth: boolean) => {
         <Route path={Paths.HOME} element={<HomePage />} />
         <Route path={Paths.MAP} element={<MapSearchPage />} />
         <Route path={`${Paths.OBJECT_PAGE}/:id`} element={<ObjectPage />} />
+        <Route path={`${Paths.CONFIRM}`} element={<ConfirmPage />} />
       </Routes>
     );
   }
