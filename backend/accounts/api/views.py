dcfbb167751b728fad00ba5d9bbabac24830b579
kindeev101048 <kindeev101048@gmail.com>
2022-02-16 11:57:01 +0300

    Code fixes

--- backend/accounts/api/views.py
+++ backend/accounts/api/views.py
@@ -216,19 +216,14 @@ class ChangePasswordView(UpdateAPIView):
         model = ClientUser
         permission_classes = (IsAuthenticated,)
 
-        def get_object(self, queryset=None):
-            obj = self.request.user
-            return obj
-
         def update(self, request, *args, **kwargs):
-            self.object = self.get_object()
             serializer = self.get_serializer(data=request.data)
 
             if serializer.is_valid():
-                if not self.object.check_password(serializer.data.get("current_password")):
+                if not request.user.check_password(serializer.data.get("current_password")):
                     return Response({'error': 'Current password is incorrect'}, status=status.HTTP_400_BAD_REQUEST)
-                self.object.set_password(serializer.data.get("new_password"))
-                self.object.save()
+                request.user.set_password(serializer.data.get("new_password"))
+                request.user.save()
                 return Response(status=status.HTTP_200_OK)
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
