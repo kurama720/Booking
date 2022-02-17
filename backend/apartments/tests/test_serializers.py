ac785bf409483f91d0f74b23c62df77814ea96f8
Viachaslau Kindzeyeu <kindeev101048@gmail.com>
2022-02-17 14:40:33 +0000

    Fix/add-is-favorite-field-to-apartments

--- backend/apartments/tests/test_serializers.py
+++ backend/apartments/tests/test_serializers.py
@@ -57,5 +57,6 @@ class ApartmentSerializerTestCase(TestCase):
             rating=None,
             business_account=test_business_client.id,
             user=[],
+            is_favorite=False,
         )
         self.assertEqual(test_data, expected_data)
