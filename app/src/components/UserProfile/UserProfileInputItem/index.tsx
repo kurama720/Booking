2fd1ae94e2cd1652974dc7a007c4fe8f88a769b3
Uladzislau Birukou <alorine99@gmail.com>
2022-02-18 09:53:02 +0000

    develop-add-change-first/last-name-functionality

--- app/src/components/UserProfile/UserProfileInputItem/index.tsx
+++ app/src/components/UserProfile/UserProfileInputItem/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Field } from "formik";
+import { BsExclamationCircleFill } from "react-icons/bs";
 import { UserProfileInputItemProps } from "./UserProfileInputItemProps";
 import UserProfileSpinner from "../UserProfileSpinner";
 
@@ -6,17 +8,33 @@ const UserProfileInputItem = ({
   value,
   handler,
   isLoaded,
+  name,
+  error,
+  touched,
+  handleBlur,
 }: UserProfileInputItemProps) => {
   return (
     <div className="relative">
-      <input
+      <Field
         className="py-[0.5rem] w-80 pl-[12px] rounded-md font-body  outline-none  text-gray-900 border border-gray-400 shadow-sm focus:border-blue-400  block sm:text-sm"
         type="text"
+        name={name}
         disabled={!isLoaded}
         value={value || ""}
-        onChange={(e) => handler(e.target.value)}
+        onBlur={handleBlur}
+        onChange={handler}
       />
       {!isLoaded && <UserProfileSpinner />}
+      {error && touched ? (
+        <span className="flex justify-start items-start text-red-600 text-xs font-body">
+          <span className="pt-0.5 pr-1 ">
+            <BsExclamationCircleFill className="text-red-600 w-3 h-3" />
+          </span>
+          {error}
+        </span>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- app/src/components/UserProfile/index.tsx
+++ app/src/components/UserProfile/index.tsx
@@ -1,17 +1,22 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { Formik, FormikHelpers, Form, useFormikContext } from "formik";
 import axios from "axios";
 import { LocalKey, LocalStorage } from "ts-localstorage";
 import UserAvatar from "../../assets/img/AvatarUserProfile.png";
 import LinkBack from "../LinkBack";
 import UserProfileInputItem from "./UserProfileInputItem";
-import { JWT } from "../UserLogInForm/utils/interfaces/interfaces";
+import { JWT, UserLogin } from "../UserLogInForm/utils/interfaces/interfaces";
 import { IUserProfile } from "./IUserProfile";
+import { validationSchema } from "./validationSchema";
+import { IUserInfo } from "./IUserInfo";
 
 const UserProfile = ({ handleChangePasswordMenu }: IUserProfile) => {
-  const [firstName, setFirstName] = useState<string>("");
-  const [lastName, setLastName] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [isLoaded, setLoaded] = useState<boolean>(false);
+  const [initialState, setInitialState] = useState<IUserInfo>({
+    firstName: "",
+    lastName: "",
+  });
 
   const getUserInfo = useCallback(async () => {
     const storageName = "userData" as LocalKey<JWT>;
@@ -21,83 +26,143 @@ const UserProfile = ({ handleChangePasswordMenu }: IUserProfile) => {
       const payload = userData.token.data.access;
       const userEmail = JSON.parse(userData.token.config.data);
       axios.defaults.headers.common.Authorization = `Bearer ${payload}`;
-
-      const userInfo = await axios.get(
-        `${process.env.REACT_APP_API_URL}accounts/info/`
-      );
-      // The userInfo may be null therefore I use if condition
-      if (userInfo) {
-        const data = JSON.parse(JSON.stringify(userInfo.data));
-        setFirstName(data.first_name);
-        setLastName(data.last_name);
-        setEmail(userEmail.email);
-        setLoaded(true);
+      try {
+        const userInfo = await axios.get(
+          `${process.env.REACT_APP_API_URL}accounts/me/`
+        );
+        // The userInfo may be null therefore I use if condition
+        if (userInfo) {
+          const data = JSON.parse(JSON.stringify(userInfo.data));
+          setEmail(userEmail.email);
+          setLoaded(true);
+          setInitialState({
+            firstName: data.first_name,
+            lastName: data.last_name,
+          });
+        }
+      } catch (e) {
+        console.log(e);
       }
     }
   }, []);
+
+  const handleFormSubmit = useCallback(
+    async (values: IUserInfo, actions: FormikHelpers<IUserInfo>) => {
+      const storageName = "userData" as LocalKey<JWT>;
+      const userData = LocalStorage.getItem(storageName);
+      const dataForSubmitting = {
+        firstName: values.firstName,
+        lastName: values.lastName,
+      };
+      // The userData may be null therefore I use if condition
+      if (userData) {
+        const payload = userData.token.data.access;
+        axios.defaults.headers.common.Authorization = `Bearer ${payload}`;
+        try {
+          const userInfo = await axios.post(
+            `${process.env.REACT_APP_API_URL}accounts/me/`,
+            dataForSubmitting
+          );
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     getUserInfo();
   }, [getUserInfo]);
 
-  const handleSubmit = () => {};
-
   return (
-    <div className="w-[62rem] shadow rounded-md py-4">
-      <div className="w-full">
-        <LinkBack text="User Profile Info" />
-        <div className="w-full h-2/5">
-          <div className="pl-20 w-full pt-6 flex">
-            <div>
-              <img
-                className="h-40 w-40 rounded-full cursor-pointer hover:bg-gray-400"
-                src={UserAvatar}
-                alt="User Avatar"
-              />
-            </div>
-            <div className="w-[74%]">
-              <div className="flex ml-2 justify-between">
-                <UserProfileInputItem
-                  value={firstName}
-                  handler={setFirstName}
-                  isLoaded={isLoaded}
-                />
-                <UserProfileInputItem
-                  value={lastName}
-                  handler={setLastName}
-                  isLoaded={isLoaded}
-                />
-              </div>
-              <div className="w-[99%] h-px bg-gray-200 mt-1 ml-2" />
-              <div className="ml-4 mt-1">
-                <p>
-                  Email:<span className="ml-2 font-body">{email}</span>
-                </p>
-                <div className="flex">
-                  <p>Password:</p>
-                  <button
-                    onClick={handleChangePasswordMenu}
-                    type="button"
-                    className="ml-2 font-body underline decoration-solid hover:text-blue-800 underline"
-                  >
-                    Click here to change
-                  </button>
+    <Formik
+      onSubmit={handleFormSubmit}
+      enableReinitialize
+      initialValues={initialState}
+      validateOnBlur
+      validationSchema={validationSchema}
+    >
+      {({
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isValid,
+        dirty,
+        values,
+      }) => (
+        <div className="w-[62rem] shadow rounded-md py-4">
+          <div className="w-full">
+            <LinkBack text="User Profile Info" />
+            <div className="w-full h-2/5">
+              <div className="pl-20 w-full pt-6 flex">
+                <div>
+                  <img
+                    className="h-40 w-40 rounded-full cursor-pointer hover:bg-gray-400"
+                    src={UserAvatar}
+                    alt="User Avatar"
+                  />
                 </div>
-              </div>
-              <div className="flex items-end justify-end w-full h-20">
-                <button
-                  onSubmit={handleSubmit}
-                  type="button"
-                  className="font-body bg-blue-600 py-2 px-6 w-20 h-10 rounded-xl text-white hover:bg-blue-800"
-                >
-                  Save
-                </button>
+                <Form onSubmit={handleSubmit} className="w-[74%]">
+                  <div className="flex ml-2 h-[55px] justify-between">
+                    <UserProfileInputItem
+                      name="firstName"
+                      error={errors.firstName}
+                      touched={touched.firstName}
+                      value={values.firstName}
+                      handler={handleChange}
+                      isLoaded={isLoaded}
+                      handleBlur={handleBlur}
+                    />
+                    <UserProfileInputItem
+                      name="lastName"
+                      value={values.lastName}
+                      error={errors.lastName}
+                      touched={touched.lastName}
+                      handler={handleChange}
+                      isLoaded={isLoaded}
+                      handleBlur={handleBlur}
+                    />
+                  </div>
+                  <div className="w-[99%] h-px bg-gray-200 ml-2" />
+                  <div className="ml-4 mt-1">
+                    <p>
+                      Email:
+                      <span className="ml-2 font-body">{email}</span>
+                    </p>
+                    <div className="flex">
+                      <p>Password:</p>
+                      <button
+                        onClick={handleChangePasswordMenu}
+                        type="button"
+                        className="ml-2 font-body underline decoration-solid hover:text-blue-800 underline"
+                      >
+                        Click here to change
+                      </button>
+                    </div>
+                  </div>
+                  <div className="flex items-end justify-end w-full h-20">
+                    <button
+                      type="submit"
+                      disabled={!dirty || !isValid}
+                      className={`font-body ${
+                        !dirty || !isValid
+                          ? "bg-gray-200 text-gray-500"
+                          : "bg-blue-600 hover:bg-blue-800 text-white"
+                      } py-2 px-6 w-20 h-10 rounded-xl leading-5 text-sm`}
+                    >
+                      Save
+                    </button>
+                  </div>
+                </Form>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Formik>
   );
 };
 
